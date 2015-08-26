57d51f03416c35e6ae8939a639f153e710705e7e
sergey.illarionov <easthunch@gmail.com>
2015-08-26 15:04:59 +0300

    add: functionality for details

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,17 @@
+'use strict';
 
+var Helper = {
+
+    init: function () {
+        Helper.toggleDetails();
+    },
+
+    toggleDetails: function () {
+        $('.details-btn').on('click', function (event) {
+            event.preventDefault();
+
+            $(this).siblings('.details').toggleClass('hidden');
+        })
+    }
+
+};
