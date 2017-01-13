f8ebdadde80780175317d483db3cfdb7f9e3b54d
Sergey.Illarionov <easthunch@gmail.com>
2017-01-13 16:39:32 +0200

    add: redux

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {Router, Route, Redirect, IndexRedirect, hashHistory} from 'react-router';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import './css/main.less';
-import App from './components/App.jsx';
-import Projects from './components/Projects.jsx';
-import Page404 from './components/Page404.jsx';
+import configureStore from './store/configureStore';
+
+import RootRoutes from './routes';
+
+if (module.hot) {
+  module.hot.accept();
+}
+
+injectTapEventPlugin();
+
+
+const store = configureStore();
 
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={App}>
-          <IndexRedirect to="/ru" />
-          <Route path="ru" component={Projects} />
-          <Route path="en" component={Projects} />
-        </Route>
-        <Route path="404" component={Page404} />
-        <Redirect from='*' to='404/' />
-    </Router>,
-    document.getElementById('main')
+  <Provider store={store}>
+    <RootRoutes />
+  </Provider>,
+  document.getElementById('main')
 );
