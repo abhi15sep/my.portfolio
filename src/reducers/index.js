41b89357dbaf9446181edba68313f504a96d61fe
Sergey.Illarionov <easthunch@gmail.com>
2017-01-16 10:00:35 +0200

    add: spinner

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
+import { pendingTasksReducer as pendingTasks } from 'react-redux-spinner';
+import locale from './locale';
 import projects from './projects';
 
 export default combineReducers({
   routing: routerReducer,
+  pendingTasks,
+  locale,
   projects
 });
