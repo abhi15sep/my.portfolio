e7bc70294a2f1a7d889f7a5b81244a31a46b6428
Sergey.Illarionov <easthunch@gmail.com>
2017-01-17 14:09:19 +0200

    refactoring: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,21 @@
 import React from 'react';
-import { Router, Route, Redirect, IndexRedirect, hashHistory } from 'react-router';
+import { Router, Route, Redirect, IndexRedirect, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
 
 import App from '../containers/App';
-import ProjectsPage from '../containers/ProjectsPage';
+import Projects from '../containers/Projects';
 import Page404 from '../components/Page404';
 
 function getHistorySyncWithStore(store) {
-  return syncHistoryWithStore(hashHistory, store);
+  return syncHistoryWithStore(browserHistory, store);
 }
 
 const RootRoutes = ({ store }) => (
   <Router history={getHistorySyncWithStore(store)}>
     <Route path="/" component={App}>
-      <IndexRedirect to="ru" />
-      <Route path="/en" component={ProjectsPage} />
-      <Route path="/ru" component={ProjectsPage} />
+      <IndexRedirect to="projects" />
+      <Route path="/projects" component={Projects} />
       <Route path="404" component={Page404} />
     </Route>
     <Redirect from="*" to="404" />
