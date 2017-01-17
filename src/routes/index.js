1d101fe39a749407bc724ba3a2021faaed8831fb
Sergey.Illarionov <easthunch@gmail.com>
2017-01-17 16:12:19 +0200

    reconfiguration: routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,7 @@ import { syncHistoryWithStore } from 'react-router-redux';
 import App from '../containers/App';
 import Projects from '../containers/Projects';
 import Page404 from '../components/Page404';
+import Unavailable from '../components/Unavailable';
 
 function getHistorySyncWithStore(store) {
   return syncHistoryWithStore(browserHistory, store);
@@ -16,8 +17,9 @@ const RootRoutes = ({ store }) => (
     <Route path="/" component={App}>
       <IndexRedirect to="projects" />
       <Route path="/projects" component={Projects} />
-      <Route path="404" component={Page404} />
     </Route>
+    <Route path="/404" component={Page404} />
+    <Route path="/unavailable" component={Unavailable} />
     <Redirect from="*" to="404" />
   </Router>
 );
